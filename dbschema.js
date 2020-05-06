09284589962f355ba1148d3e3ef3e65085dfedd1
aleksandar7 <aleks.djekic@gmail.com>
2020-05-06 23:09:50 +0100

    post lesson 10

--- dbschema.js
+++ dbschema.js
@@ -21,4 +21,29 @@ let db = {
         commentCount: 2
         }
     ]
-}
+};
+const userDetails = {
+  credentials: [
+      {
+        userId: 'dh23ggj5h32g543j5gf43',
+        email: 'user@email.com',
+        handle: 'user',
+        createdAt: '2019-03-15T10:59:52.798Z',
+        imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+        bio: 'Hello, my name is user, nice to meet you',
+        website: 'https://user.com',
+        location: 'Lonodn, UK'
+      }
+    ],
+  likes: [
+      {
+      userHandle: 'user',
+      screamID: 'dh23ggj5h32g543j5gf43'
+      },
+      {
+      userHandle: 'user',
+      screamID: 'dh23ggj5h32g543j5gf43'
+      }
+      
+  ]
+};
